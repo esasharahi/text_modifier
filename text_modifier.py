06568d6cddd0a5c5c2db273306355639d5478e04
esasharahi <esasharahi@gmail.com>
2016-10-08 21:11:00 +0330

    Create text_modifier.py

---
+++ text_modifier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+## esasharahi@gmail.com
+import re
+import os.path
+def main():
+	input_file = input("Text file address: ")
+	with open(input_file) as f:
+		file_for_modify = f.read()
+		modified_file = open("modified_" + os.path.basename(input_file), 'w')
+		for x in ",.:;?!،؛":
+			bad_regex = r" *" + re.escape(x) + r" *"
+			p = re.compile(bad_regex)
+			m = p.sub(x + r" " , file_for_modify)
+			file_for_modify = m
+		modified_file.write(m)
+main() 
