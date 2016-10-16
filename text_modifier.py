191aad5861b5252ae2a9ac3ecbdeff9053f7d025
esasharahi <esasharahi@gmail.com>
2016-10-16 15:01:26 +0330

    Update text_modifier.py

--- text_modifier.py
+++ text_modifier.py
@@ -7,10 +7,22 @@ def main():
 	with open(input_file) as f:
 		file_for_modify = f.read()
 		modified_file = open("modified_" + os.path.basename(input_file), 'w')
+		for x in "([{":
+			bad_regex = r" *" + re.escape(x) + r" *"
+			p = re.compile(bad_regex)
+			m = p.sub(r" " + x , file_for_modify)
+			file_for_modify = m
+			
+		for x in ")]}":
+			bad_regex = r" *" + re.escape(x) + r" *"
+			p = re.compile(bad_regex)
+			m = p.sub(x + r" " , file_for_modify)
+			file_for_modify = m
+			
 		for x in ",.:;?!،؛":
 			bad_regex = r" *" + re.escape(x) + r" *"
 			p = re.compile(bad_regex)
 			m = p.sub(x + r" " , file_for_modify)
 			file_for_modify = m
 		modified_file.write(m)
-main() 
+main()
